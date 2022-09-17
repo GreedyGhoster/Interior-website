6005f0932bdbb39885f716d87e34fa9c9b84e745
Ivan Nikitin <vananikitin95@gmail.com>
2022-09-17 21:46:16 +0300

    Add js(calculator)

--- learn.js
+++ learn.js
@@ -1,1 +1,28 @@
-alert("Не нажимайте OK!!! Иначе будет...");
+"use strick";
+const calc = function () {
+  let a = prompt("Введите первое число");
+  let b = prompt("Введите второе число");
+  let c = prompt("Введите знак операции (*, /, +, -, **)");
+  if (isFinite(a) && isFinite(b) && (c == "/" || "*" || "**" || "+" || "-")) {
+    let x;
+    if (c == "+") {
+      x = a + b;
+    } else if (c == "-") {
+      x = a - b;
+    } else if (c == "*") {
+      x = a * b;
+    } else if (c == "**") {
+      x = a ** b;
+    } else if (c == "/") {
+      if (b == 0) {
+        x = "На 0 делить нельзя!";
+      } else {
+        x = a / b;
+      }
+    }
+    alert(`Ваш ответ: ${x}`);
+  } else {
+    alert("Введите число и знак операции!!!");
+  }
+};
+calc();
