9bb0b26a06f83fdb9a8b02c211fee8981b50e903
Ivan Nikitin <vananikitin95@gmail.com>
2022-09-29 21:41:50 +0300

    Add js code in learn.js

--- learn.js
+++ learn.js
@@ -1,28 +1,74 @@
 "use strick";
-const calc = function () {
-  let a = prompt("Введите первое число");
-  let b = prompt("Введите второе число");
-  let c = prompt("Введите знак операции (*, /, +, -, **)");
-  if (isFinite(a) && isFinite(b) && (c == "/" || "*" || "**" || "+" || "-")) {
-    let x;
-    if (c == "+") {
-      x = a + b;
-    } else if (c == "-") {
-      x = a - b;
-    } else if (c == "*") {
-      x = a * b;
-    } else if (c == "**") {
-      x = a ** b;
-    } else if (c == "/") {
-      if (b == 0) {
-        x = "На 0 делить нельзя!";
-      } else {
-        x = a / b;
-      }
-    }
-    alert(`Ваш ответ: ${x}`);
-  } else {
-    alert("Введите число и знак операции!!!");
-  }
-};
-calc();
+// const calc = function () {
+//   let a = prompt("Введите первое число");
+//   let c = prompt("Введите знак операции (*, /, +, -, **)");
+//   let b = prompt("Введите второе число");
+//   if (isFinite(a) && isFinite(b) && (c == "/" || "*" || "**" || "+" || "-")) {
+//     let x;
+//     if (c == "+") {
+//       x = a + b;
+//     } else if (c == "-") {
+//       x = a - b;
+//     } else if (c == "*") {
+//       x = a * b;
+//     } else if (c == "**") {
+//       x = a ** b;
+//     } else if (c == "/") {
+//       if (b == 0) {
+//         alert("На 0 делить нельзя!");
+//       } else {
+//         x = a / b;
+//       }
+//     }
+//     alert(`Ваш ответ: ${x}`);
+//   } else {
+//     alert("Введите число и знак операции!!!");
+//   }
+// };
+// calc();
+
+// const sum = function (x, y) {
+//   return x + y;
+// };
+// sum(sum(15, 15), sum(20, 20));
+
+// let summ = function (a, b) {
+//   return a + b;
+// };
+// let diff = function (x, y) {
+//   return x - y;
+// };
+// let sud = function (func) {
+//   let c = 5;
+//   let i = 2;
+//   return c + i;
+// };
+// alert(sud(diff));
+// alert(sud(summ));
+
+// (function () {
+//   alert("AAAAAAA");
+// })();
+
+// let x = "h t t p : / /w w w .w illia m sp u b lish in g .c o m /B o o k s/9 7 8 -5 ~ 9 0 7 2 0 3 -7 9 -2 .h tm l";
+// alert(x.split(" ").join(""));
+
+// let Me = {
+//   age: 15,
+//   weight: 54,
+//   width: 40,
+//   height: 170,
+// };
+// Me.eyes = "Gray-Green";
+// alert(Me.eyes);
+
+// let points = [1, 2, 3, 4, 5];
+// alert(points[1] - points[0]);
+
+// const Sum1 = (x) => x - 1;
+// alert(Sum1(5));
+// const Sum2 = (y) => y + 1;
+// alert(Sum2(3));
+
+// let x = 3;
+// x == 3 ? alert("hh") : alert("gg");
